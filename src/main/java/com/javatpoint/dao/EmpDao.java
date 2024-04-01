41292a717b3b11802987d2fa63872a199ba1965c
UserName-Mr-Mahesh <maheshlucky525@gmail.com>
2024-04-01 14:32:18 +0530

    added

---
+++ src/main/java/com/javatpoint/dao/EmpDao.java
@@ -0,0 +1,66 @@
+package com.javatpoint.dao;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import com.javatpoint.beans.Emp;
+
+public class EmpDao {
+	JdbcTemplate template;
+
+	public void setTemplate(JdbcTemplate template) {
+		this.template = template;
+	}
+
+	public int save(Emp p) {
+		String sql = "insert into emp99(name,salary,designation) values('" + p.getName() + "'," + p.getSalary() + ",'"
+				+ p.getDesignation() + "')";
+		return template.update(sql);
+	}
+
+	public int update(Emp p) {
+		String sql = "update emp99 set name='" + p.getName() + "', salary=" + p.getSalary() + ",designation='"
+				+ p.getDesignation() + "' where id=" + p.getId() + "";
+		return template.update(sql);
+	}
+
+	public int delete(int id) {
+		String sql = "delete from emp99 where id=" + id + "";
+		return template.update(sql);
+	}
+
+	public Emp getEmpById(int id) {
+		String sql = "select * from emp99 where id=?";
+		return template.queryForObject(sql, new Object[] { id }, new BeanPropertyRowMapper<Emp>(Emp.class));
+	}
+
+	public List<Emp> getEmployees() {
+		return template.query("select * from emp99", new RowMapper<Emp>() {
+			public Emp mapRow(ResultSet rs, int row) throws SQLException {
+				Emp e = new Emp();
+				e.setId(rs.getInt(4));
+				e.setName(rs.getString(1));
+				e.setSalary(rs.getFloat(2));
+				e.setDesignation(rs.getString(3));
+				return e;
+			}
+		});
+	}
+
+	public List<Emp> getEmployeesByPage(int pageid, int total) {
+		String sql = "select * from emp99 limit " + (pageid - 1) + "," + total;
+		return template.query(sql, new RowMapper<Emp>() {
+			public Emp mapRow(ResultSet rs, int row) throws SQLException {
+				Emp e = new Emp();
+				e.setId(rs.getInt(4));
+				e.setName(rs.getString(1));
+				e.setSalary(rs.getFloat(2));
+				e.setDesignation(rs.getString(3));
+				return e;
+			}
+		});
+	}
+}
